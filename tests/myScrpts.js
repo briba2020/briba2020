eeae7f0a7f90dbc74d2b4d9ca31357446e99badd
briba2020 <55292397+briba2020@users.noreply.github.com>
2020-01-15 13:09:36 -0500

    Update myScrpts.js

--- tests/myScrpts.js
+++ tests/myScrpts.js
@@ -1,1 +1,32 @@
-
+var i;
+for( i=0; i<31; i++){
+  document.write("yeeep" + i + "<br/>");
+}
+var j;
+for( j = 200;j > 150; j--){
+  document.write("la" + j + "<br/>");
+}
+var f;
+for(f = 0; f < 101; f=f+2){
+  document.write("nope"+ f + "<br/>");
+}
+var g;
+for(g = 100; g < 5000; g=g+100){
+  document.write("skeep"+ g + "<br/>");
+}
+var m; 
+for(m=0; m<9; m++){
+  var ans= Math.pow(2,m );
+  document.write(ans + "<br/>");
+}
+var k; 
+for(k=0; k<1; k++){
+  var ans= Math.sqrt(256);
+  document.write(ans + "<br/>");
+}
+var v=2
+while(v < 8){
+  var ans= Math.random(v);
+  document.write(ans + "<br/>");
+  v++;
+}
